cd3ee40014eb0cd216bbe1d4913daf46fea0cd9b
minikryc <mini122600@gmail.com>
2024-05-20 11:36:59 +0900

    test

---
+++ src/main/java/com/example/clotherbe/ClotherBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.clotherbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClotherBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClotherBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clotherbe/ClotherBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clotherbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClotherBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
